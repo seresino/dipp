e6c9526719b27d6e1555d20c97e5dafe39e33c6b
Ruby <rubyseresin@gmail.com>
2024-01-28 14:12:42 +0000

    task headings displaying but ui semi finished

--- src/routes/instructions/+page.server.ts
+++ src/routes/instructions/+page.server.ts
@@ -1,22 +1,26 @@
 import db from "$lib/server/db";
-import { modules } from "$lib/server/schema";
+import { modules, tasks } from "$lib/server/schema";
 import { eq } from "drizzle-orm";
 
 import { getDay, getModuleID } from "$lib/utils/helperFunctions";
 
-// Would acc import these in from somewhere else --------------------------------
 const loggedInUserID = 2;
 const moduleID = getModuleID();
 
 export const load = async () => {
-	// Load in module name
-	// Load in daily-task entry for that user for today
+	// Load in module info
 	const moduleQuery = await db
 		.select()
 		.from(modules)
 		.where(eq(modules.id, moduleID));
 
+	const tasksQuery = await db
+		.select()
+		.from(tasks)
+		.where(eq(tasks.module_id, moduleID));
+
 	return {
 		module: moduleQuery[0],
+		tasks: tasksQuery,
 	};
 };
--- src/routes/instructions/+page.svelte
+++ src/routes/instructions/+page.svelte
@@ -7,6 +7,7 @@
   let selectedButton = $page.url.searchParams.get('initial') || 'instructions';
 
   const module = data.module;
+  const tasks = data.tasks;
 </script>
 
 <div class="pop-up">
@@ -24,17 +25,62 @@
       </button>
     </div>
     {#if selectedButton === 'instructions'}
+      <h1>Module {module.id}: {module.name}</h1>
       <p>{module.instructions}</p>
     {:else if selectedButton === 'tasks'}
-      <div>
-        <p>{module.tasks}</p>
-      </div>
+
+      {#each tasks as task, index}
+        <div class="task-shape">
+          <img src="/images/task-shape.svg" alt="task-shape" />
+          <div class="task-shape-content">
+            <div class="task-number-box">{index + 1}</div>
+            <div class="task-shape-text">
+              <h2>{task.task}</h2>
+              <p>{task.goal}</p>
+            </div>
+          </div>
+        </div>
+      {/each}
     {/if}
   </div>
 </div>
 
 
 <style>
+  .task-shape {
+    position: relative;
+  }
+  .task-shape-content {
+    display: flex;
+    flex-direction: row;
+    position:absolute;
+    top:0; bottom:0; left:0; right:0;
+    padding: 20px;
+  }
+  h2 {
+    font-size: 20px;
+    font-weight: normal;
+  }
+  .task-number-box {
+    background-color: #5DB3E5;
+    width: 50px;
+    height: 100%;
+    border: 1px solid #168ACE;
+    border-radius: 10px;
+    box-shadow: 0px 4px 13.1px -4px rgba(0, 0, 0, 0.25) inset;
+    font-size: 30px;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .task-shape-text {
+    display: flex;
+    flex-direction: column;
+    justify-content: top;
+    align-items: left;
+  }
+
   .back-button {
     position: absolute;
     width: 40px;
