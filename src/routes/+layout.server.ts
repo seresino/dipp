0ad59587b98497bb50003ee141fd4fa052262058
codehath <codingfarhath@gmail.com>
2024-02-21 20:17:20 +0000

    # Removed log

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,6 @@
 // get `locals.user` and pass it to the page
 export const load = async ({ locals }) => {
-  console.log("load fn run");
-  return {
-    user: locals.user,
-  };
+	return {
+		user: locals.user,
+	};
 };
