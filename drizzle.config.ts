50247b46f112eb538dbaaa956c97f51d8ff7848b
codehath <codingfarhath@gmail.com>
2024-10-16 03:03:54 +0100

    Added DATABASE_PORT to .env for new drizzle config implementation

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -15,12 +15,15 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import { defineConfig } from "drizzle-kit";
 
+const db_url =
+	"postgres://${process.env.VITE_DATABASE_USERNAME}:${process.env.VITE_DATABASE_PASSWORD}@${process.env.VITE_DATABASE_HOST}:${process.env.VITE_DATABASE_PORT}/${process.env.VITE_DATABASE_NAME}";
+// console.log("URL", db_url);
+
 export default defineConfig({
 	out: "./drizzle",
 	schema: "./src/lib/server/schema.ts",
 	dialect: "postgresql",
 	dbCredentials: {
-		url: "postgres://farhath:@localhost:5432/dipp",
-		// url: "postgres://${process.env.VITE_DATABASE_USERNAME}:${process.env.VITE_DATABASE_PASSWORD}@${process.env.VITE_DATABASE_HOST}:${process.env.VITE_DATABASE_PORT}/${process.env.VITE_DATABASE_NAME}",
+		url: db_url,
 	},
 });
