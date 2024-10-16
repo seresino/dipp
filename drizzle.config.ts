dc3ad81e8a351778b75c9fd6b23d42e5acf6145d
codehath <codingfarhath@gmail.com>
2024-10-16 05:55:25 +0100

    Removed commented code in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,23 +1,9 @@
-// import type { Config } from "drizzle-kit";
-// import * as dotenv from "dotenv";
-// dotenv.config();
-
-// export default {
-// 	schema: "./src/lib/server/schema.ts",
-// 	out: "./migrations",
-// 	driver: "pg",
-// 	dbCredentials: {
-// 		connectionString: `postgres://${process.env.VITE_DATABASE_USERNAME}:${process.env.VITE_DATABASE_PASSWORD}@${process.env.VITE_DATABASE_HOST}/${process.env.VITE_DATABASE_NAME}`,
-// 	},
-// } satisfies Config;
-
 import * as dotenv from "dotenv";
 dotenv.config();
 import { defineConfig } from "drizzle-kit";
 
 const db_url =
 	"postgres://${process.env.VITE_DATABASE_USERNAME}:${process.env.VITE_DATABASE_PASSWORD}@${process.env.VITE_DATABASE_HOST}:${process.env.VITE_DATABASE_PORT}/${process.env.VITE_DATABASE_NAME}";
-// console.log("URL", db_url);
 
 export default defineConfig({
 	out: "./drizzle",
