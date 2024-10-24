53f33300e8a241f82f0d5fb5ac866176d4d29340
codehath <codingfarhath@gmail.com>
2024-10-24 22:28:20 +0100

    Replaced sql with configured pool in db.ts - added connection string

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,7 +1,31 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import { env } from "$env/dynamic/private";
+// // 1
+// import { drizzle } from "drizzle-orm/postgres-js";
+// import postgres from "postgres";
+// import { env } from "$env/dynamic/private";
 
-const client = postgres(env.VITE_DB_URL);
-const db = drizzle(client);
+// const client = postgres(env.VITE_DB_URL!);
+// const db = drizzle(client);
+// export default db;
+
+// 2
+// import { drizzle } from "drizzle-orm/vercel-postgres";
+// import { sql } from "@vercel/postgres";
+
+// // import { config } from 'dotenv';
+// // config({ path: '.env.local' }); // or .env
+
+// // export const db = drizzle(sql);
+
+// const db = drizzle(sql);
+// export default db;
+
+// 3
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { createPool } from "@vercel/postgres";
+
+const pool = createPool({
+	connectionString: process.env.VITE_DB_URL,
+});
+
+const db = drizzle(pool);
 export default db;
