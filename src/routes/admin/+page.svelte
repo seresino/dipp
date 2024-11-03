cc2c5535910b36b30125e9b4ca772a4704cc0724
codehath <codingfarhath@gmail.com>
2024-11-03 11:02:50 +0000

    # Styled Admin page

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -22,9 +22,8 @@
 	}
 </script>
 
-<h1>Admin</h1>
 
-<h4>Add new participant</h4>
+<h4>Add New Participant:</h4>
 {#if form?.message}
 	<p>{form.message}</p>
 {/if}
@@ -33,9 +32,11 @@
 	<input type="text" name="username" placeholder="Enter username..." />
 	<input type="text" name="password" placeholder="Enter password..." />
 	<input type="date" name="startDate" placeholder="Enter start date..." />
-	<label for="meditation">Meditation?</label>
-	<input type="checkbox" id="meditation" name="meditation" />
-	<input type="submit" value="Add" />
+	<div class="checkbox-container">
+		<label for="meditation">Meditation Group</label>
+		<input type="checkbox" id="meditation" name="meditation" />
+	</div>
+	<input type="submit" value="Add Participant" />
 </form>
 
 <h4>Download Data</h4>
@@ -47,6 +48,59 @@
 <button on:click={exportTables} class="styled-button">Export Tables</button>
 
 <style>
+	/* General page styles */
+	h1 {
+		font-size: 2em;
+		margin-bottom: 0.5em;
+		color: #333;
+	}
+
+	h4 {
+		font-size: 1.5em;
+		margin-top: 1.5em;
+		color: #555;
+	}
+
+	form {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5em;
+		margin-top: 1em;
+	}
+
+	input[type="text"],
+	input[type="date"],
+	input[type="submit"] {
+		padding: 0.5em;
+		font-size: 1em;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+	}
+
+	input[type="submit"] {
+		background-color: #28a745;
+		color: white;
+		cursor: pointer;
+		width: 100%;
+	}
+
+	input[type="submit"]:hover {
+		background-color: #218838;
+	}
+
+	input[type="submit"]:active {
+		background-color: #1e7e34;
+	}
+
+	.checkbox-container {
+		display: flex;
+		gap: 0.5em;
+	}
+
+	input[type="checkbox"] {
+		transform: scale(1.5);
+	}
+
 	/* Styles for the buttons */
 	.styled-button {
 		padding: 0.5em 1em;
@@ -57,10 +111,13 @@
 		cursor: pointer;
 		border-radius: 4px;
 		margin-top: 1em;
+		width: 185px;
 	}
+
 	.styled-button:hover {
 		background-color: #0056b3;
 	}
+
 	.styled-button:active {
 		background-color: #004085;
 	}
